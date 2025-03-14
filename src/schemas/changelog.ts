d1611a71fd9840e8e42bd3e2cabc26a784a6a5ee
kodster28 <kody@cloudflare.com>
2025-03-14 13:08:30 -0500

    add noindex

--- src/schemas/changelog.ts
+++ src/schemas/changelog.ts
@@ -14,4 +14,10 @@ export const changelogSchema = ({ image }: SchemaContext) =>
 				"An array of products to associate this changelog entry with. You may omit the product named after the folder this entry is in.",
 			),
 		preview_image: image().optional(),
+		noindex: z
+			.boolean()
+			.optional()
+			.describe(
+				"If true, this property adds a `noindex` declaration to the page, which will tell internal / external search crawlers to ignore this page. Helpful for pages that are historically accurate, but no longer recommended, such as [Workers Sites](/workers/configuration/sites/).",
+			),
 	});
